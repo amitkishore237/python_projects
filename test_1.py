5683f879431fd0e9e98067d7149103254d2a0cca
amit.kishore <amit.kishore@innovaccer.com>
2019-06-28 01:50:00 +0530

    Day 1 print your name

--- test_1.py
+++ test_1.py
@@ -1,6 +1,6 @@
 print("hello everyone welcome to day 1")
 #let's print your name
-print("what is your name:")
+print("what is your sexy name:")
 print(" Amit")
 # print your name using for loop
 list_names=["name_1","name_2","name_3"]
